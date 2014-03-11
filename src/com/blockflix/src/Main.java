d9bfa853749d137af4b5572e81cda5281166c7b6
Toni <toni.amate@gmail.com>
2014-03-11 13:50:27 +0100

    Prueba 2

---
+++ src/com/blockflix/src/Main.java
@@ -0,0 +1,15 @@
+package com.blockflix.src;
+
+public class Main {
+
+	public static enum EstadoProducto { ALQUILADO, DISPONIBLE, NO_DISPONIBLE};
+
+	public static enum TipoProducto { PELICULA, SERIE, MUSICA};
+
+	public static enum Soporte { DVD, BLURAY, CD, VINILO};
+	
+	 
+
+
+
+}
---
+++ src/com/blockflix/src/Producto.java
@@ -0,0 +1,13 @@
+package com.blockflix.src;
+
+
+import com.blockflix.src.Main.*;
+public class Producto {
+
+		private String nombre;
+		private String categoria;
+		private EstadoProducto estado;
+		private TipoProducto tipo;
+		
+		
+}
